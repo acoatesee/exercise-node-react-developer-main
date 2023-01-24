855735387bed340172bd21109360371741969d43
Alex Coates <dev.acoates@gmail.com>
2023-01-23 18:40:23 -0700

    refactor- api uses AppError [DRY]

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -1,80 +1,42 @@
 import express, { Router, Request, Response } from 'express';
 import axios from 'axios';
-// import dataRepos from '../../data/repos.json'; //only read on server start
+import { AppError } from '../models/AppError';
+import { debug } from 'console';
 export const repos = Router();
 
 //I'm not sure if express.static caches content on first load
 repos.use(express.static('data'));
 
-repos.get('/', async (req, res) => {
+repos.get('/', async (req, res, next) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
 
-  const remoteRepos = await getGithubRepos();
-  const localRepos = await getServerRepos();
+  try {
+    const remoteRepos = await getRepos(
+      'http://localhost:4000/repos/repos.json' // to prevent github api abuse
+    );
+    const localRepos = await getRepos('http://localhost:4000/repos/repos.json');
+    const filteredCollection = processRepos([...remoteRepos, ...localRepos]);
 
-  if (remoteRepos.status !== 200) {
-    res.status(remoteRepos.status);
-    res.json({ message: remoteRepos.message });
+    res.json(filteredCollection);
+  } catch (error) {
+    next(error);
   }
-  if (localRepos.status !== 200) {
-    res.status(localRepos.status);
-    res.json({ message: localRepos.message });
-  }
-
-  const filteredCollection = processRepos([
-    ...remoteRepos.data,
-    ...localRepos.data,
-  ]);
-
-  res.json(filteredCollection);
 });
 
-/**
- * Access remote repositories from github api
- * @returns {status: "code", data: "request data", message: "response context"}
- */
-async function getGithubRepos() {
+async function getRepos(path) {
+  debug(`Making request to ${path}`);
   try {
-    const remoteRepos = await axios(
-      'https://api.github.com/users/silverorange/repos'
-    );
-    return {
-      status: 200,
-      data: remoteRepos.data,
-      message: `Successfully retrieved github repos`,
-    };
+    return await (
+      await axios.get(path)
+    ).data;
   } catch (error) {
-    return {
-      status: error.status,
-      data: error.data,
-      message: `Failed to retrieve repos from github`,
-    };
-  }
-}
-
-/**
- * Access local repositories from data
- * @returns {status: "code", data: "request data", message: "response context"}
- *
- */
-async function getServerRepos() {
-  try {
-    const localRepos = await axios.get(
-      'http://localhost:4000/repos/repos.json'
+    throw new AppError(
+      `Failed to retrieve repos from '${path}'`,
+      // axios errors status path
+      error?.response?.status ?? 500
     );
-    return {
-      status: 200,
-      data: localRepos.data,
-      message: 'Successfully retrieved local repos',
-    };
-  } catch (error) {
-    return {
-      status: error.status,
-      data: error.data,
-      message: `Failed to retrieve repos from local server data`,
-    };
   }
 }
 
