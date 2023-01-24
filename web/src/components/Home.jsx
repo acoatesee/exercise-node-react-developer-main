13b649f086ee5e9e39bd76df7fceec4eb0490d4d
Alex Coates <dev.acoates@gmail.com>
2023-01-23 18:36:20 -0700

    feat- loads repos on 200 status

--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+// on load get repos from backend then display content
 export default function Home() {
-  return <div>Home</div>;
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function loadRepos() {
+    setError('');
+    setLoading(true);
+    try {
+      const response = await (
+        await axios.get('http://localhost:4000/repos/')
+      ).data;
+      setRepos(() => JSON.stringify(response));
+    } catch (err) {
+      setError(() => err.message);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadRepos();
+  }, []);
+  return (
+    <div>
+      <strong>Repositories for silverorange</strong>
+      <br />
+      {loading && 'loading'}
+      {error}
+      {repos}
+    </div>
+  );
 }
